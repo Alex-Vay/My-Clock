ed3350e81daa575c561f66ed587f985e13bb9c4b
Sanechekpirojochek <sasha.ignatov93@gmail.com>
2024-04-07 22:54:13 +0500

    фрагмент для добавления будильника и изменения его настроек, на который должен быть переход после нажатия кнопки add alarm, но пока переход не работает

--- app/src/main/java/com/nr/myclock/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nr/myclock/ui/home/HomeFragment.kt
@@ -1,21 +1,23 @@
 package com.nr.myclock.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.nr.myclock.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,11 +28,22 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val root: View = binding.root
+//        Кнопка для добавления будильника
+        val addAlarm: Button = binding.alarmButton
+//      не получается добавить фрагмент(alarmsettingsfragment.xml) который будет отображаться после нажатия кнопки
+//        addAlarm.setOnClickListener {
+//            // Создать и открыть новый фрагмент для настройки будильника
+//            val newFragment = AlarmSettingsFragment() // здесь AlarmSettingsFragment - это ваш новый фрагмент для настройки будильника
+//            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, newFragment).commit()
+//
+//        }
+
+        val textView: TextView = binding.alarmButton
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+
         }
         return root
     }
--- app/src/main/java/com/nr/myclock/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/nr/myclock/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is alarms Fragment"
+//        value = "This is alarms Fragment"
     }
     val text: LiveData<String> = _text
 }
